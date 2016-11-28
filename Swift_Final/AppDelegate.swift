bf5f7229c077886e144d78e4f88e38d64d557ef2
Melissa Bower <melissabower@melissas-macbook-air.local>
2016-11-27 17:06:58 -0700

    Initial Commit

---
+++ Swift_Final/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Swift_Final
+//
+//  Created by Melissa Bower on 11/27/16.
+//  Copyright © 2016 TeamSwift. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Swift_Final/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Swift_Final
+//
+//  Created by Melissa Bower on 11/27/16.
+//  Copyright © 2016 TeamSwift. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
